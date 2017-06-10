ab6dfd4f9b84d99a645cec666745b071bf76c67e
gongcy <gongcy126@126.com>
2017-06-10 16:52:43 +0800

    添加项目文件。

---
+++ File2No/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace File2No
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textInp = new System.Windows.Forms.TextBox();
+            this.btnInp = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(445, 108);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(134, 29);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "选择文件夹";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(445, 160);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(134, 33);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "导出XLS";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "作业文件夹路径";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textPath
+            // 
+            this.textPath.Location = new System.Drawing.Point(134, 113);
+            this.textPath.Name = "textPath";
+            this.textPath.Size = new System.Drawing.Size(292, 21);
+            this.textPath.TabIndex = 4;
+            this.textPath.TextChanged += new System.EventHandler(this.textPath_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "花名册文件";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textInp
+            // 
+            this.textInp.Location = new System.Drawing.Point(134, 57);
+            this.textInp.Name = "textInp";
+            this.textInp.Size = new System.Drawing.Size(292, 21);
+            this.textInp.TabIndex = 3;
+            // 
+            // btnInp
+            // 
+            this.btnInp.Location = new System.Drawing.Point(445, 52);
+            this.btnInp.Name = "btnInp";
+            this.btnInp.Size = new System.Drawing.Size(134, 29);
+            this.btnInp.TabIndex = 0;
+            this.btnInp.Text = "选择花名册文件";
+            this.btnInp.UseVisualStyleBackColor = true;
+            this.btnInp.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 237);
+            this.Controls.Add(this.btnInp);
+            this.Controls.Add(this.textInp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSelect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "提交作业登记";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textInp;
+        private System.Windows.Forms.Button btnInp;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+    }
+}
+
---
+++ File2No/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace File2No
+{
+    public partial class Form1 : Form
+    {
+        IWorkbook wk = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        // btnSelect
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                textPath.Text = foldPath;
+            }
+        }
+
+        // btnExport
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textPath.Text.Length == 0)
+                {
+                    throw new Exception();
+                }
+                DirectoryInfo theFolder = new DirectoryInfo(textPath.Text);
+                if (!theFolder.Exists)
+                {
+                    throw new Exception();
+                }
+                FileInfo[] fileinfo = theFolder.GetFiles();
+   
+                string extension = System.IO.Path.GetExtension(textInp.Text);
+                FileStream fs = File.OpenRead(textInp.Text);
+                if (extension.Equals(".xls"))
+                {
+                    // for .xls
+                    wk = new HSSFWorkbook(fs);
+                }
+                else
+                {
+                    // for .xlsx
+                    wk = new XSSFWorkbook(fs);
+                }
+                fs.Close();
+                ISheet sheet = wk.GetSheetAt(0);
+
+                for (int i = 0; i < fileinfo.Length; i++)
+                {
+                    Regex reg = new Regex("201\\d+");
+                    if (reg.IsMatch(fileinfo[i].Name))
+                    {
+                        string No = reg.Match(fileinfo[i].Name).ToString();
+                        searchAndWrite(sheet, No);
+                    }
+                }
+                sheet.AutoSizeColumn(0);
+                fs = File.OpenWrite(textInp.Text);
+                wk.Write(fs);
+                fs.Flush();
+                fs.Close();
+                MessageBox.Show(null, "导出成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(null, "导出失败！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.Out.WriteLine(ex.Message);
+            }
+        }
+
+        private void searchAndWrite(ISheet sheet, string No)
+        {
+            ICellStyle style = wk.CreateCellStyle();
+            style.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Center;
+            style.ShrinkToFit = true;
+
+            for (int i = 0; i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row != null)
+                {
+                    for (int j = 0; j <= row.LastCellNum; j++)
+                    {
+                        ICell cell = row.GetCell(j);
+                        if (cell != null)
+                        {
+                            string value = cell.ToString();
+                            if (value.Equals(No))
+                            {
+                                cell = row.CreateCell(row.LastCellNum);
+                                cell.SetCellValue("Y");
+                                cell.CellStyle = style;
+                                // cell.SetCellType(CellType.String);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // btnInp
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Title = "请选择花名册文件";
+            od.Filter = "EXCEL文件(*.xls)|*.xls|EXCEL文件(*.xlsx)|*.xlsx";
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo theFile = new FileInfo(od.FileName);
+                string filePath =theFile.FullName;
+                textInp.Text = filePath;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(null, "点名程序\n如有问题，请联系gongcy126@126.com", "关于", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
